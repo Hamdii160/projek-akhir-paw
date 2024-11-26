c42cdca4109e959b0eb9ca7cd4ee190e23266395
AchmadFauzanAlif <alif.fitra1511@gmail.com>
2024-11-26 17:50:17 +0700

    perbaruan tampilan login

--- db.php
+++ db.php
@@ -1,14 +1,35 @@
 <?php
 // Database connection
+// $host = 'localhost';
+// $dbname = 'wisata';
+// $user = 'root';
+// $pass = '';
+
+
+
+// try {
+//     $conn = new PDO($host,$dbname, $user, $pass);
+//     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+// } catch (PDOException $e) {
+//     die("Connection failed: " . $e->getMessage());
+// }
+
+?>
+
+<?php
+// Database connection
 $host = 'localhost';
 $dbname = 'wisata';
 $user = 'root';
 $pass = '';
 
-try {
-    $conn = new PDO($host,$dbname, $user, $pass);
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-} catch (PDOException $e) {
-    die("Connection failed: " . $e->getMessage());
+// Buat koneksi ke database
+$conn = mysqli_connect($host, $user, $pass, $dbname);
+
+// Periksa koneksi
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
 }
+
+echo "Connected successfully";
 ?>
